6ae2fa0521467fb5170e3cb6444a3968e461e6ec
Michael O%27Brien <mobrien@colgate.edu>
2018-04-24 22:52:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-var a, b
-
-beforeEach(function() {
-  a = Math.floor(Math.random() * 1000)
-  b = Math.floor(Math.random() * 1000)
-})
-
 function add(a, b) {
-  a + b
+  (a + b)
 }
 
 function subtract(a, b) {
